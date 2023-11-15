d6db14bee81b8d46fa6f61f244d98ca097a2c84b
Christopher Kelly <cck197@cck197.net>
2023-11-15 05:42:33 -0800

    whoop server logging

--- whoop/app.py
+++ whoop/app.py
@@ -5,7 +5,12 @@ from flask import Flask, request
 
 from sleepmate.db import *
 from sleepmate.helpful_scripts import setup_logging
-from sleepmate.whoop import WHOOP_TOKEN_URL, get_whoop_oauth2_session, get_whoop_token
+from sleepmate.whoop import (
+    WHOOP_CLIENT_ID,
+    WHOOP_TOKEN_URL,
+    get_whoop_oauth2_session,
+    get_whoop_token,
+)
 
 load_dotenv()
 
@@ -14,6 +19,28 @@ app = Flask(__name__)
 setup_logging()
 
 log = logging.getLogger("whoop.app")
+
+log.info(f"starting WHOOP server {WHOOP_CLIENT_ID=}")
+
+
+def log_request():
+    headers = "\n".join(f"{k}: {v}" for k, v in request.headers.items())
+    log_message = f"Request {request.method} {request.url}\nHeaders:\n{headers}"
+
+    if request.method == "POST":
+        if request.json:
+            post_data = request.json
+            log_message += f"\nJSON POST Data: {post_data}"
+        elif request.form:
+            post_data = request.form
+            log_message += f"\nForm POST Data: {post_data}"
+
+    log.info(log_message)
+
+
+@app.before_request
+def before_request():
+    log_request()
 
 
 @app.route("/whoop_update", methods=["POST"])
