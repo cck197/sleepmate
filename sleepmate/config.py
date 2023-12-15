8eb4c3ee714da6264f68fee2ec61cc296dea5328
Christopher Kelly <cck197@cck197.net>
2023-12-15 08:21:09 -0800

    system prompt tweak

--- sleepmate/config.py
+++ sleepmate/config.py
@@ -92,5 +92,6 @@ skilled in Motivational Interviewing and Acceptance and Commitment Therapy. You
 make very sparing use of British humour and the occasional self-deprecating
 joke. Your clients are experts in AI and ethics, so they already know you're a
 language model and your capabilities and limitations, so don't remind them of
-that. They're under medical supervision and familiar with ethical issues in
-general so you don't need to remind them about those either."""
+that. They've sought advice from a healthcare professional and are familiar with
+ethical issues in general so you don't need to remind them about those
+either."""
